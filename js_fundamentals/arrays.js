9aae7b0a76272a79d7ca2da3dc3d2cbc12bd3f57
elmatematic0 <wkdalebout+github@gmail.com>
2013-11-21 18:20:21 -0500

    Completing the array exercise.

--- js_fundamentals/arrays.js
+++ js_fundamentals/arrays.js
@@ -7,4 +7,12 @@
 // Bonus: Change it to log "My 1st choice, "My 2nd choice", "My 3rd choice", picking the right suffix for the number based on what it is.
 
 
+var topChoices = ["camels", "llamas", "donkeys"];
+var indexVal = 0
+while(indexVal<=topChoices.length-1){
+console.log("My #" + (indexVal+1) + " choice, " + topChoices[indexVal] + ".");
+indexVal++;
+}
+
+
 
